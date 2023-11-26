b6b67a48d2306efb2c14eb7cd65775caf75ad3ee
Vitugome3s <victor.aguiar08@hotmail.com>
2023-11-26 06:28:23 -0300

    Update funcoes.c

--- funcoes.c
+++ funcoes.c
@@ -1,11 +1,47 @@
+#include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include <strings.h>
 #include "funcoes.h"
 
 void menu()
 {
 }
 
-void criar_tabela()
+int criar_tabela(int x)
 {
+    char name[30];
+    printf("Qual será o nome de sua tabela?");
+    scanf("%s", name);
+    strcat(name,".txt");
+    int key;
+    int count = x;
+    FILE *arquivo;
+    // Abre o arquivo para escrita (w)
+    arquivo = fopen(name, "w");
+    // Verifica se o arquivo foi aberto com sucesso
+    if (arquivo == NULL)
+    {
+        fprintf(stderr, "Erro ao abrir o arquivo.\n");
+        return 1;
+    }
+    else{
+        char aux ='s';
+        while (aux == 's')
+        {
+        count++;
+        printf("Qual a chave primária?");
+        scanf("%d", &key);
 
+        // Escreve no arquivo
+        fprintf(arquivo, "%d | ", key);
+        
+        printf("Deseja adicionar outra chave? s ou n?");
+        scanf("%s", &aux);
+        }
+    }
+        // Fecha o arquivo
+        fclose(arquivo);
+        return 0;
+    
 }
