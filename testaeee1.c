19aa57719cb7c2bc3d6c02ec4cedaebc4ae3e7db
Vitugome3s <victor.aguiar08@hotmail.com>
2023-11-25 22:28:18 -0300

    Create testaeee1.c

---
+++ testaeee1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int linhas, inicial = 1;
+
+//definir a quantidade de linhas
+
+    scanf("%d", &linhas);
+    
+    if(linhas== 0){
+        printf("Você entrou com 0, tente de novo na próxima");
+    }
+
+//construção do triangulo de floyd
+
+    for (int i = 1; i <= linhas; i++) {
+        for (int j = 1; j <= i; j++) {
+            printf("%2d ", inicial);
+            inicial++;
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
