742899d70a9b1d998367f27656e5874bed987546
gabrielle borja <gabrielle.borja.091@ufrn.edu.br>
2023-11-26 02:55:36 -0300

    Co-authored-by: Vitugome3s <Vitugome3s@users.noreply.github.com>

--- funcoes.c
+++ funcoes.c
@@ -4,5 +4,35 @@ void menu(){
     
 }
 
+void criar_tabela(){
+    //nome da tabela
+    char name[30];
+    //chave primária
+    int key;
 
+    //nome da tabela
+    printf("Qual será o nome de sua tabela?");
+    scanf("%s",&name);
+
+    //primeira chave primária
+    printf()
+
+
+
+    FILE *arquivo;
+    // Abre o arquivo para escrita (w)
+    arquivo = fopen("exemplo.txt", "w");
+
+    // Verifica se o arquivo foi aberto com sucesso
+    if (arquivo == NULL) {
+        fprintf(stderr, "Erro ao abrir o arquivo.\n");
+    }
+    // Escreve no arquivo
+    fprintf(arquivo, "Olá, mundo!\n");
+
+    // Fecha o arquivo
+    fclose(arquivo);
+
+    
+}
 
