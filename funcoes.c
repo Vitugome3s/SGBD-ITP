a29a0c356de8c2a3c0ead64dfc2366b2f21a0c5c
Vitugome3s <victor.aguiar08@hotmail.com>
2023-11-26 17:42:53 -0300

    Grandão

--- funcoes.c
+++ funcoes.c
@@ -5,18 +5,61 @@
 #include "funcoes.h"
 
 void menu()
-{  
+{
+    int choice;
+
+    printf("================= MENU ================= \n"
+           "Escolha qual função você quer digitando: \n"
+           "1-Criar uma tabela\n"
+           "2-Listar todas as tabelas\n"
+           "3-Criar uma linha\n"
+           "4-Listar dados de uma tabela\n"
+           "5-Pesquisar valor em uma tabela\n"
+           "6-Apagar uma tupla (registro ou linha) de uma tabela\n"
+           "7-Apagar uma tabela\n"
+           "0-Sair do menu\n");
+
+    scanf("%d", &choice);
+    switch (choice)
+    {
+    case 1:
+        criar_tabela();
+        break;
+    case 2:
+        listar_tabelas();
+        break;
+    case 3:
+        criar_linha();
+        break;
+    case 4:
+        listar_dados();
+        break;
+    case 5:
+        pesquisar_valor();
+        break;
+    case 6:
+        apagar_tupla();
+        break;
+    case 7:
+        apagar_tabela();
+        break;
+    case 0:
+        break;
+    default:
+        printf("Escolha inválida.\n");
+        break;
+    }
 }
 
-<<<<<< Victor
-int criar_tabela(int x)
+int criar_tabela()
 {
+    // Escolhe o nome da tabela
     char name[30];
     printf("Escolha o nome de sua tabela:");
     scanf("%s", name);
     strcat(name, ".txt");
     int key;
-    int count = x;
+    int count = 0;
     FILE *arquivo;
     // Abre o arquivo para escrita (w)
     arquivo = fopen(name, "w");
@@ -36,7 +79,7 @@ int criar_tabela(int x)
             scanf("%d", &key);
 
             // Escreve no arquivo
-            fprintf(arquivo, "%d | ", key);
+            fprintf(arquivo, "%d\n", key);
 
             printf("Deseja adicionar outra chave? s ou n?");
             scanf("%s", &aux);
@@ -46,7 +89,27 @@ int criar_tabela(int x)
     fclose(arquivo);
     return 0;
 }
-=======
 
+void listar_tabelas()
+{
+}
 
->>>>>> main
+void criar_linha()
+{
+}
+
+void listar_dados()
+{
+}
+
+void pesquisar_valor()
+{
+}
+
+void apagar_tupla()
+{
+}
+
+void apagar_tabela()
+{
+}
--- funcoes.h
+++ funcoes.h
@@ -1,7 +1,19 @@
 #ifndef FUNCOES_H_INCLUDED
 #define FUNCOES_H_INCLUDED
 
-int criar_tabela(int);
+int criar_tabela(void);
+
+void listar_tabelas(void);
+
+void criar_linha(void);
+
+void listar_dados(void);
+
+void pesquisar_valor(void);
+
+void apagar_tupla(void);
+
+void apagar_tabela(void);
 
 void menu(void);
 
