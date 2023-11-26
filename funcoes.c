355f41b04befd7e587ceb8ef302f0dea0b014c81
Vitugome3s <victor.aguiar08@hotmail.com>
2023-11-26 14:16:14 -0300

    Update funcoes.c

--- funcoes.c
+++ funcoes.c
@@ -11,37 +11,39 @@ void menu()
 int criar_tabela(int x)
 {
     char name[30];
-    printf("Qual será o nome de sua tabela?");
+    printf("Escolha o nome de sua tabela:");
+    char nome_arquivo[100] = "/arquivos/";
     scanf("%s", name);
-    strcat(name,".txt");
+    strcat(nome_arquivo, name);
+    strcat(nome_arquivo, ".txt");
     int key;
     int count = x;
     FILE *arquivo;
     // Abre o arquivo para escrita (w)
-    arquivo = fopen(name, "w");
+    arquivo = fopen(nome_arquivo, "w");
     // Verifica se o arquivo foi aberto com sucesso
     if (arquivo == NULL)
     {
         fprintf(stderr, "Erro ao abrir o arquivo.\n");
         return 1;
     }
-    else{
-        char aux ='s';
+    else
+    {
+        char aux = 's';
         while (aux == 's')
         {
-        count++;
-        printf("Qual a chave primária?");
-        scanf("%d", &key);
+            count++;
+            printf("Qual a chave primária?");
+            scanf("%d", &key);
 
-        // Escreve no arquivo
-        fprintf(arquivo, "%d | ", key);
-        
-        printf("Deseja adicionar outra chave? s ou n?");
-        scanf("%s", &aux);
+            // Escreve no arquivo
+            fprintf(arquivo, "%d | ", key);
+
+            printf("Deseja adicionar outra chave? s ou n?");
+            scanf("%s", &aux);
         }
     }
-        // Fecha o arquivo
-        fclose(arquivo);
-        return 0;
-    
+    // Fecha o arquivo
+    fclose(arquivo);
+    return 0;
 }
