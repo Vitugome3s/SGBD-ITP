a58ab2b70b52dcb0aabf190662b94eb58cf0b76a
Vitugome3s <victor.aguiar08@hotmail.com>
2023-11-28 23:54:15 -0300

    restore

--- funcoes.c
+++ funcoes.c
@@ -9,6 +9,7 @@ void menu()
     int choice;
 
     printf("================= MENU ================= \n"
+<<<<<<< Updated upstream
            "Escolha qual função você quer digitando: \n"
            "1-Criar uma tabela\n"
            "2-Listar todas as tabelas\n"
@@ -18,6 +19,17 @@ void menu()
            "6-Apagar uma tupla (registro ou linha) de uma tabela\n"
            "7-Apagar uma tabela\n"
            "0-Sair do menu\n");
+=======
+           "       Escolha a funcao desejada: \n"
+           "1 - Criar uma tabela\n"
+           "2 - Listar todas as tabelas\n"
+           "3 - Criar uma linha\n"
+           "4 - Listar dados de uma tabela\n"
+           "5 - Pesquisar valor em uma tabela\n"
+           "6 - Apagar uma tupla (registro ou linha) de uma tabela\n"
+           "7 - Apagar uma tabela\n"
+           "0 - Sair do menu\n");
+>>>>>>> Stashed changes
 
     scanf("%d", &choice);
     switch (choice)
@@ -51,9 +63,14 @@ void menu()
     }
 }
 
+<<<<<<< Updated upstream
 int criar_tabela()
 {
     // Escolhe o nome da tabela
+=======
+void criar_tabela(){
+    //Variáveis
+>>>>>>> Stashed changes
     char name[30];
     printf("Escolha o nome de sua tabela:");
     scanf("%s", name);
@@ -67,7 +84,6 @@ int criar_tabela()
     if (arquivo == NULL)
     {
         fprintf(stderr, "Erro ao abrir o arquivo.\n");
-        return 1;
     }
     else
     {
@@ -85,6 +101,7 @@ int criar_tabela()
             scanf("%s", &aux);
         }
     }
+<<<<<<< Updated upstream
     // Fecha o arquivo
     fclose(arquivo);
     return 0;
@@ -92,6 +109,32 @@ int criar_tabela()
 
 void listar_tabelas()
 {
+=======
+    //Fecha o file
+    fclose(file);
+    printf("\n");
+    menu();
+}
+
+void listar_tabelas(){
+    struct dirent *dirp;
+    directory = opendir("arquivos");
+    if(directory==NULL){
+        printf("\nNão há nenhuma tabela registrada.\n");
+    }
+    else{ 
+        printf("\nTabelas registradas:\n");
+        while(dirp = readdir(directory)){
+            if (strcmp(dirp->d_name, ".") != 0 && strcmp(dirp->d_name, "..") != 0) {
+            char *table_name = strtok(dirp->d_name, ".");
+            printf("%s\n", table_name);
+            }
+        }
+    }
+    closedir(directory);
+    printf("\n");
+    menu();
+>>>>>>> Stashed changes
 }
 
 void criar_linha()
