105e52e2ad5992bd6673ac1e4d6099d856531734
gabrielle borja <gabrielle.borja.091@ufrn.edu.br>
2023-11-28 22:38:40 -0300

    update criar tabela

--- funcoes.c
+++ funcoes.c
@@ -4,20 +4,23 @@
 #include <strings.h>
 #include "funcoes.h"
 
+FILE *file;//arquivo para escrita da tabela 
+FILE *table;//arquivo com nome de todas as tabelas
+
 void menu()
 {
     int choice;
 
     printf("================= MENU ================= \n"
-           "Escolha qual função você quer digitando: \n"
-           "1-Criar uma tabela\n"
-           "2-Listar todas as tabelas\n"
-           "3-Criar uma linha\n"
-           "4-Listar dados de uma tabela\n"
-           "5-Pesquisar valor em uma tabela\n"
-           "6-Apagar uma tupla (registro ou linha) de uma tabela\n"
-           "7-Apagar uma tabela\n"
-           "0-Sair do menu\n");
+           "       Escolha a função desejada: \n"
+           "1 - Criar uma tabela\n"
+           "2 - Listar todas as tabelas\n"
+           "3 - Criar uma linha\n"
+           "4 - Listar dados de uma tabela\n"
+           "5 - Pesquisar valor em uma tabela\n"
+           "6 - Apagar uma tupla (registro ou linha) de uma tabela\n"
+           "7 - Apagar uma tabela\n"
+           "0 - Sair do menu\n");
 
     scanf("%d", &choice);
     switch (choice)
@@ -53,40 +56,57 @@ void menu()
 
 int criar_tabela()
 {
-    // Escolhe o nome da tabela
+    //Variáveis
     char name[30];
-    printf("Escolha o nome de sua tabela:");
-    scanf("%s", name);
-    strcat(name, ".txt");
     int key;
     int count = 0;
-    FILE *arquivo;
-    // Abre o arquivo para escrita (w)
-    arquivo = fopen(name, "w");
-    // Verifica se o arquivo foi aberto com sucesso
-    if (arquivo == NULL)
-    {
-        fprintf(stderr, "Erro ao abrir o arquivo.\n");
+    char aux = 's';
+    char column[50];
+    int *choice[20];
+
+    //Nome da tabela
+    printf("Escolha o nome de sua tabela:");
+    scanf("%s", name); 
+
+    //Abre arquivos e insere nome da tabela
+    table = fopen("tabelas.txt", "a");
+    fprintf(table, "%s\n", name);
+    fclose(table);
+    strcat(name, ".txt");
+    file = fopen(name, "w");  
+
+    //Verifica se o arquivo foi aberto com sucesso
+    if (file == NULL){
+        fprintf(stderr, "Erro ao abrir o file.\n");
         return 1;
     }
-    else
-    {
-        char aux = 's';
+    else{
+        //Chave primária da tabela
+        printf("Qual a chave primária?");
+        scanf("%d", &key);    
+        fprintf(file, "%d  ", key);  //Escreve no arquivo
+
         while (aux == 's')
         {
-            count++;
-            printf("Qual a chave primária?");
-            scanf("%d", &key);
-
-            // Escreve no arquivo
-            fprintf(arquivo, "%d\n", key);
-
-            printf("Deseja adicionar outra chave? s ou n?");
+            printf("Digite o nome da coluna:");
+           
+            scanf("%s", &column);
+            fprintf(file, "| %s ", column);
+            printf("Qual o tipo dos dados da coluna?\n"
+                    "1 - int\n"
+                    "2 - float\n"
+                    "3 - char\n"
+                    "4 - double\n"
+                    "5 - string\n");
+            scanf("%d", &choice[count]);
+            fprintf(file, "(%d) ", choice[count]);
+            count++; //Conta colunas
+            printf("Deseja adicionar outra coluna? s ou n?");
             scanf("%s", &aux);
         }
     }
-    // Fecha o arquivo
-    fclose(arquivo);
+    //Fecha o file
+    fclose(file);
     return 0;
 }
 
