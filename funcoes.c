480d9084016df6953a95b69778fe877a4d0eed7e
gabrielle borja <gabrielle.borja.091@ufrn.edu.br>
2023-12-13 00:28:15 -0300

    update

--- funcoes.c
+++ funcoes.c
@@ -20,37 +20,91 @@ void menu()
            "5 - Pesquisar valor em uma tabela\n"
            "6 - Apagar uma tupla (registro ou linha) de uma tabela\n"
            "7 - Apagar uma tabela\n"
-           "0 - Sair do menu\n");
+           "0 - Fechar\n");
     scanf("%d", &choice);
     switch (choice)
     {
     case 1:
         criar_tabela();
+        menu();
         break;
     case 2:
         listar_tabelas();
+        menu();
         break;
     case 3:
         criar_linha();
+        menu();
         break;
     case 4:
         listar_dados();
+        menu();
         break;
     case 5:
         pesquisar_valor();
+        menu();
         break;
     case 6:
         apagar_tupla();
+        menu();
         break;
     case 7:
         apagar_tabela();
+        menu();
         break;
     case 0:
         break;
     default:
         printf("Escolha inválida.\n");
+        menu();
         break;
     }
+}
+
+int check_tablename(char tablename[30])
+{
+    struct dirent *dirp;
+    int count = 0;
+    directory = opendir("arquivos");
+    if (directory == NULL) return 1;
+    else{
+        while (dirp = readdir(directory)){
+            if (strcmp(dirp->d_name, ".") != 0 && strcmp(dirp->d_name, "..") != 0){
+                char *table_name = strtok(dirp->d_name, ".");
+                if(strcmp(tablename, table_name)==0){
+                    count++;
+                }
+            }
+        }
+        closedir(directory);
+        if(count != 0) return 1;
+        else return 0;
+    } 
+}
+
+int check_linepk(char name[30], int key){
+    FILE *file = fopen(name, "r");
+    if (file == NULL) {
+        // Tratar erro ao abrir o arquivo, se necessário
+        perror("Erro ao abrir o arquivo");
+        return -1; 
+    }
+
+    int pkLida;
+    char linha[1000];
+    
+    while (fgets(linha, sizeof(linha), file) != NULL) {
+        sscanf(linha, "%d", &pkLida);
+
+        // Se a PK for igual 
+        if (pkLida == key) {
+            fclose(file);
+            return 0;
+        }
+    }
+
+    fclose(file);
+    return 1;
 }
 
 void criar_tabela()
@@ -65,6 +119,12 @@ void criar_tabela()
     // Nome da tabela
     printf("Escolha o nome de sua tabela:");
     scanf("%s", &name);
+    while(check_tablename(name)==1){
+        printf("Erro! Ja existe uma tabela com esse nome.\n");
+        printf("Escolha o nome de sua tabela: ");
+        scanf("%s", &name);
+        check_tablename(name);
+    }
     // Abre pasta e arquivo
     char drctry[20] = "arquivos/";
     strcat(name, ".txt");
@@ -74,6 +134,8 @@ void criar_tabela()
     if (file == NULL)
     {
         fprintf(stderr, "Erro ao abrir o arquivo.\n");
+        printf("\n");
+        menu();
     }
     else
     {
@@ -81,11 +143,7 @@ void criar_tabela()
         num_column = 0;
         fprintf(file, "Número de colunas:%d\n",num_column);
 
-        // Chave primária da tabela
-
-        printf("Qual a chave primária?");
-        scanf("%d", &key);
-        fprintf(file, "%*d  ", 4, key); // Escreve no arquivo
+        fprintf(file, "PK  ", key); // Escreve no arquivo
 
         while (aux == 's')
         {
@@ -120,6 +178,7 @@ void criar_tabela()
                 break;
             default:
                 printf("Erro! Opção não disponível\n");
+                menu();
                 break;
             }
             // Conta colunas
@@ -138,7 +197,6 @@ void criar_tabela()
     // Fecha o file
     fclose(file);
     printf("\n");
-    menu();
 }
 
 void listar_tabelas()
@@ -148,6 +206,8 @@ void listar_tabelas()
     if (directory == NULL)
     {
         printf("\nNão há nenhuma tabela registrada.\n");
+        printf("\n");
+        menu();
     }
     else
     {
@@ -163,7 +223,6 @@ void listar_tabelas()
     }
     closedir(directory);
     printf("\n");
-    menu();
 }
 
 void criar_linha()
@@ -172,7 +231,7 @@ void criar_linha()
     int num_column;
     char table_line[30];
     char aux = 's';
-        
+
     printf("Em qual tabela deseja criar uma linha?");
     scanf("%s", table_line);
     char new_table_line[30] = "arquivos/";
@@ -180,75 +239,266 @@ void criar_linha()
     strcat(new_table_line, table_line);
     file = fopen(new_table_line, "r+");
 
-        if (file == NULL){
-            fprintf(stderr, "Erro ao abrir o arquivo.\n");
+    if (file == NULL)
+    {
+        fprintf(stderr, "Erro ao abrir o arquivo.\n");
+    }
+    else
+    {
 
+        fseek(file, 0, SEEK_SET); // Ir para a parte inicial do arquivo
+
+        fscanf(file, "Número de colunas:%d", &num_column); // Ler o número de colunas
+
+        char name[num_column][30];
+        char type[num_column][30];
+        fscanf(file, " PK ");
+        for (int i = 0; i < num_column; i++)
+        {
+            fscanf(file, " | %s (%s)", name[i], type[i]);
         }
-        else{
+        while (aux == 's')
+        {
+            fseek(file, 0, SEEK_END); // ir para o final do documento
 
-            fseek(file,0, SEEK_SET);           //Ir para a parte inicial do arquivo
-
-            fscanf(file, "Número de colunas:%d", &num_column);     //Ler o número de colunas 
-
-
-            char name[num_column][30];
-            char type[num_column][30];
-            fscanf(file,"%*d");
-            for(int i=0;i<num_column;i++){
-                fscanf(file, " | %s (%s)", name[i], type[i]);
-            }
-            while (aux == 's')
-            {
-                fseek(file, 0, SEEK_END); // ir para o final do documento
-
-                //Chave primária da linha
-                char new_line[30];
+            // Chave primária da linha
+            char new_line[30];
+            printf("Qual a chave primaria?");
+            scanf("%d", &key);
+            while(check_linepk(new_table_line, key) == 0){
+                printf("Erro! Chave primaria ja utilizada.\n");
                 printf("Qual a chave primaria?");
                 scanf("%d", &key);
-                fprintf(file, "\n%*d ", 4, key);
+            }
+            fprintf(file, "\n%*d ", 4, key);
 
-                // Loop para pegar e escrever os dados na tabela
-                for (int i = 0; i < num_column; i++) {
-                    int len_cell = (strlen(name[i]) + strlen(type[i])+2);
-                    printf("Digite um(a) %s do tipo (%s: ", name[i], type[i]);
-                    scanf("%s",new_line); //CONFERIR TIPO
-                    fprintf(file, " | %*s",len_cell, new_line);  // Adiciona o valor no arquivo
+            // Loop para pegar e escrever os dados na tabela
+            for (int i = 0; i < num_column; i++)
+            {
+                int len_cell = strlen(name[i]) + strlen(type[i]) + 2;
+                char new_line[len_cell];
+                printf("Digite um(a) %s do tipo (%s): ", name[i], type[i]);
+                scanf("%s", new_line);
+                fprintf(file, " | %*s", len_cell, new_line); // Adiciona o valor no arquivo
             }
             printf("Deseja adicionar outra linha? s ou n?\n");
             scanf("%s", &aux);
         }
-        }
+    }
 
     fclose(file);
     printf("\n");
-    menu();
 }
 
 void listar_dados()
 {
+    char caractere;
     char table_line[30];
     printf("Qual tabela você deseja imprimir os dados?");
     scanf("%s", table_line);
     char new_table_line[30] = "arquivos/";
     strcat(table_line, ".txt");
     strcat(new_table_line, table_line);
-    file = fopen(new_table_line, "r");
+    file = fopen(new_table_line, "r+");
 
-        if (file == NULL){
-            fprintf(stderr, "Erro ao abrir o arquivo.\n");
-        }
-        else{
-        
+    if (file == NULL)
+    {
+        fprintf(stderr, "Erro ao abrir o arquivo.\n");
     }
+    else
+    {
+        fseek(file, 21, SEEK_CUR);
+        while ((caractere = fgetc(file)) != EOF)
+        {
+            printf("%c", caractere);
+        }
+    }
+    fclose(file);
+    printf("\n\n");
 }
 
-void pesquisar_valor()
+void listar_dados_p(char nome[30])
 {
+    char caractere;
+    file = fopen(nome, "r+");
+
+    if (file == NULL)
+    {
+        fprintf(stderr, "Erro ao abrir o arquivo.\n");
+    }
+    else
+    {
+        fseek(file, 21, SEEK_CUR);
+        while ((caractere = fgetc(file)) != EOF)
+        {
+            printf("%c", caractere);
+        }
+    }
+    fclose(file);
+    printf("\n\n");
+}
+
+void pesquisar_valor(){
+//     int num_column;
+//     char table_line[30];
+//     int choice;
+//     char choice_2;
+//     printf("Qual tabela voce deseja pesquisar os dados? ");
+//     scanf("%s", table_line);
+//     char new_table_line[30] = "arquivos/";
+//     strcat(table_line, ".txt");
+//     strcat(new_table_line, table_line);
+//     file = fopen(new_table_line, "r");
+
+//     if (file == NULL){
+//         fprintf(stderr, "Erro ao abrir o arquivo.\n");
+//         printf("\n");
+//         menu();
+//     }
+//     else{
+//         fseek(file,0, SEEK_SET);
+
+//         fscanf(file, "Número de colunas:%d", &num_column);     //Ler o número de colunas 
+
+//         char name[num_column][30];
+//         char type[num_column][30];
+
+//         fscanf(file,"%*d");
+        
+//         for(int i=0;i<num_column;i++){
+//             fscanf(file, " | %s (%s)", name[i], type[i]);
+//         }
+//         printf("Colunas disponiveis para pesquisa:\n");
+//         for (int i = 0; i < num_column; i++) {
+//             int count = i+1;
+//             printf("%d - %s\n", count, name[i]);
+//         }
+
+//         int choice_column;
+//         printf("Digite o numero da coluna desejada: ");
+//         scanf("%d", &choice_column);
+//         char data_c[30];
+//         char data_s[30][30];
+//         int tempora;
+//         int data_i[30];
+//         float data_f[30];
+//         double data_lf[30];
+//         int count_c = 0; int count_s = 0; int count_i = 0; int count_f = 0; int count_lf = 0;
+//         char linha[1000]; 
+//         while(fgets(linha, sizeof(linha), file) != NULL){
+//             fscanf(file,"%*d ");
+//             for(int i=0;i<(choice_column-1);i++){
+//                 fscanf(file, "|%*s");   
+//             }
+//             int k = choice_column - 1;
+//             switch(type[k][0]){
+//                     case 'c' : fscanf(file, "|%c",   data_c[count_c]); count_c++; break;
+//                     case 's' : fscanf(file, "|%s",   data_s[count_s]); count_s++; break;
+//                     case 'i' : fscanf(file, "|%d",   tempora); data_i[count_i] = tempora; count_i++; break;
+//                     case 'f' : fscanf(file, "|%f",   &data_f[count_f]); count_f++; break;
+//                     case 'd' : fscanf(file, "|%lf",  &data_lf[count_lf]); count_lf++; break;
+//                  }
+                 
+//             int d = num_column - choice_column;
+//             for(int j=0;j<d;j++){
+//                 fscanf(file, "|%*s");
+//             }
+//             printf("%d",*data_i[0]);
+//             printf("%d",data_i[1]);
+//             printf("%d",data_i[2]);
+//             printf("%d",data_i[3]);
+//             printf("%d",data_i[4]);
+//         }
+
+//         printf("\n  ===== Escolha uma das opcoes de pesquisa =====\n"
+//                "1 - maior que o valor informado\n"
+//                "2 - maior ou igual que o valor informado\n"
+//                "3 - igual o valor informado\n" 
+//                "4 - menor que o valor informado\n"
+//                "5 - menor ou igual que o valor informado\n"
+//                "6 - proximo ao valor informado (apenas para strings)\n");
+//         printf("Escolha: ");
+//         scanf("%d", &choice);
+        
+//         int a = choice_column - 1;
+//         if(strcmp(type[a], "char)") == 0){
+//             char search[30];
+//             printf("\nDigite o valor desejado: ");
+//             scanf("%s", &search);
+    
+//         }
+//         else{
+//             if (choice == 6){
+//                 printf("Erro! Opcao valida apenas para pesquisa com dados do tipo string");
+//                 pesquisar_valor();
+//             }
+//             else{
+//             double search;
+//             printf("\nDigite o valor desejado: ");
+//             scanf("%lf", &search);
+//             }
+//         }
+//     }
 }
 
 void apagar_tupla()
 {
+    FILE *file, *temp;
 
+    // Pegar o nome da tabela que deseja abrir.
+    char Table_name[30];
+    printf("Em qual tabela deseja apagar uma tupla?");
+    scanf("%s", &Table_name);
+    char new_table_name[30] = "arquivos/";
+    strcat(Table_name, ".txt");
+    strcat(new_table_name, Table_name);
+
+    // Perguntar qual chave primária deseja apagar.
+    printf("Qual chave primaria deseja apagar? ");
+    int chavePrimaria;
+    scanf("%d", &chavePrimaria);
+
+    // Abre o arquivo no modo leitura e escrita
+    char linha[1000];
+    file = fopen(new_table_name, "r");
+    if (file == NULL)
+    {
+        printf("Erro ao abrir o arquivo");
+        exit(EXIT_FAILURE);
+    }
+
+    // Cria um arquivo temporário no modo escrita
+    temp = fopen("temp.txt", "w");
+    if (temp == NULL)
+    {
+        printf("Erro ao criar arquivo temporario");
+        fclose(file);
+        exit(EXIT_FAILURE);
+    }
+
+    // Lê e escreve linha por linha, ignorando a linha com a chave primária
+    int pkLida;
+    while (fgets(linha, sizeof(linha), file) != NULL)
+    {
+        sscanf(linha, "%d", &pkLida);
+
+        // Se a linha não contiver a chave primária, escreva no arquivo temporário
+        if (pkLida != chavePrimaria)
+        {
+            fputs(linha, temp);
+        }
+    }
+
+    // Fecha os arquivos abertos
+    fclose(file);
+    fclose(temp);
+
+    // Remove o arquivo original e renomear o arquivo temporário
+    remove(new_table_name);
+    rename("temp.txt", new_table_name);
+
+    printf("Tupla com chave primaria %d removida com sucesso.\n", chavePrimaria);
+    printf("\n\n");
 }
 
 void apagar_tabela()
@@ -276,5 +526,6 @@ void apagar_tabela()
     if(count==0){
         printf("Tabela não existente.\n");
         listar_tabelas();
+        menu();
     }
 }
